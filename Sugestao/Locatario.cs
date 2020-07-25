1a6bca162fafaced4079459431797f85fec7d285
Fernando Flores <fernandodeabreuflores35@gmail.com>
2020-07-25 08:41:12 -0300

    Projeto Aluguel

---
+++ Sugestao/Locatario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sugestao
+{
+    class Locatario
+    {
+        string nome;
+        string email;
+
+        public Locatario(string nome, string email)
+        {
+            this.nome = nome;
+            this.email = email;
+        }
+    }
+}
---
+++ Sugestao/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Sugestao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Locatario[] vect = new Locatario[10];
+
+            Console.WriteLine("How many rooms will be rented?");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Rent{i}");
+                Console.WriteLine("Name: ");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("e-mail: ");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Room: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vect[quarto] = new Locatario(nome, email);
+            }
+            { 
+
+                Console.WriteLine();
+                Console.WriteLine("Occupied rooms: ");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (vect[i] != null)
+                    {
+                        Console.WriteLine(i+": "+vect[i]);
+                    }
+                }
+
+            }
+
+        }
+    }
+}
